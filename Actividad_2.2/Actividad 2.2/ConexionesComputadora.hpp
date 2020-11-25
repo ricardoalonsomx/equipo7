fcb6c5fa2d0a626b5f8f67e14321af4f101eed98
ArturoSosaC <A01022687@itesm.mx>
2020-11-24 21:03:40 -0600

    Añadir Actividad_5.2

---
+++ Actividad_2.2/Actividad 2.2/ConexionesComputadora.hpp
@@ -0,0 +1,94 @@
+//
+//  ConexionesComputadora.hpp
+//  Actividad 2.2
+//
+//  Created by Arturo Sosa on 13/10/20.
+//  Copyright © 2020 Arturo Sosa. All rights reserved.
+//
+
+#ifndef ConexionesComputadora_hpp
+#define ConexionesComputadora_hpp
+#include <stdio.h>
+#include "Registro.hpp"
+#include "Queue/Queue.hpp"
+#include "Stack/Stack.hpp"
+using namespace std;
+
+
+template <class T>
+class ConexionesComputadora {
+private:
+    int ip;
+    string nombre;
+    Stack<T> ConexionesEntrantes;
+    Queue<T> ConexionesSalientes;
+    
+public:
+    ConexionesComputadora() {};
+    ConexionesComputadora(int Ip, string Nombre, Stack<string> Entrantes,
+                          Queue<string> Salientes) {
+        ip = Ip;
+        nombre = Nombre;
+        ConexionesEntrantes = Entrantes;
+        ConexionesSalientes = Salientes;
+    }
+    
+    /* Setters */
+    void setIp(int Ip){
+        ip =Ip;
+    }
+    void setNombre(string Nombre) {
+        nombre = Nombre;
+    }
+    
+    
+    /* Getters */
+    int getIp(int Ip){
+        return ip;
+    }
+    string getNombre(string Nombre) {
+        return nombre;
+    }
+    
+    
+    
+    void insertConexionesEntrantes(const T & value) {
+        ConexionesEntrantes.push(value);
+    }
+    void insertConexionesSaientes(const T & value) {
+        ConexionesSalientes.enqueue(value);
+    }
+    void insertConexionesSaientesFront(const T & value) {
+        ConexionesSalientes.reverse();
+        ConexionesSalientes.enqueue(value);
+        ConexionesSalientes.reverse();
+    }
+    
+    int sizeOfConexionesEntrantes(){
+            return ConexionesEntrantes.size();
+        }
+        
+    int sizeOfConexionesSalientes(){
+        return ConexionesSalientes.size();
+        }
+    
+    T getConexionesEntrantes() {
+        T value;
+        //value = *ConexionesEntrantes.top() -> getInfo();
+        ConexionesEntrantes.pop();
+        
+        return value;
+    }
+    T getConexionesSalientes() {
+        return ConexionesSalientes.dequeue() -> getInfo();
+    }
+    
+    void clear(){
+        ConexionesEntrantes.clear();
+        ConexionesSalientes.clear();
+    }
+
+};
+
+
+#endif /* ConexionesComputadora_hpp */
